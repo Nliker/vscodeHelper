26482cb3c2e79d3e99f796555635148959544111
unknown <tjwjdgus83@naver.com>
2024-07-07 16:58:08 +0900

    크롤링을 통해 키,명령,명령 ID 추출를 dict로 추출

---
+++ crawll.py
@@ -0,0 +1,45 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import json
+
+response = requests.get("https://demun.github.io/vscode-tutorial/shortcuts/")
+soup = BeautifulSoup(response.text, 'html.parser')
+data = {}
+
+# dict형식에 맞도록 데이터 추출
+# {
+#     "동작":[
+#        { 
+#             "키":"...",
+#             "명령":"...",
+#             "명령ID":"..."
+#         }
+#     ]
+# }
+
+h2_tags = soup.find_all('h2')
+for h2 in h2_tags:
+    key = h2.text.strip()  # h2 태그의 텍스트를 key로 사용
+    data[key] = []
+
+    # 현재 h2 태그의 다음 sibling이 테이블인 경우 데이터 추출
+    table = h2.find_next_sibling('table')
+    if table:
+        tbody = table.find('tbody')
+        if tbody:
+            rows = tbody.find_all('tr')
+            for row in rows:
+                columns = row.find_all('td')
+                value1 = columns[0].text.strip()
+                value2 = columns[1].text.strip()
+                value3 = columns[2].text.strip()
+                if value1 == "할당되지 않음":
+                    continue
+                data[key].append({
+                    "키": value1,
+                    "명령": value2,
+                    "명령 ID": value3
+                })
+
+
